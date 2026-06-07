51a6a8c77d3c8599985777b6a2c57aca82efde02
Rona Godilo <ronagodilo05@gmail.com>
2026-06-07 20:17:15 +0800

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,15 @@
+# utils.py
+
+BG_COLOR = "#EAF7FF"
+CARD_COLOR = "#FFFFFF"
+PRIMARY = "#2196F3"
+SECONDARY = "#64B5F6"
+TEXT = "#1F2937"
+SUCCESS = "#00C853"
+
+FONT_TITLE = ("Arial", 24, "bold")
+FONT_SUB = ("Arial", 12)
+FONT_BUTTON = ("Arial", 12, "bold")
+FONT_TEXT = ("Arial", 13)
+
+WINDOW_SIZE = "420x650"
